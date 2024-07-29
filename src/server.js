7d9ceb6264c28dd607b97b64fd6f34779134e3b9
Zainab Choudhry <zainabchoudhry05@gmail.com>
2024-07-29 14:48:16 +0100

    Added core functionality

--- src/server.js
+++ src/server.js
@@ -8,6 +8,8 @@ const cors = require("cors");
 //Create a new express application
 const app = express();
 
+let counter = 0;
+
 //Tell express we want to use the morgan library
 app.use(morgan("dev"));
 //Tell express we want to use the cors library
@@ -15,5 +17,28 @@ app.use(cors());
 //Tell express to parse JSON in the request body
 app.use(express.json());
 
+app.get("/counter", function(req, res) {
+    res.status(200).json({counter: counter});
+})
+
+app.delete("/counter", function(req, res) {
+    counter = 0
+    res.status(200).json({ counter: counter });
+})
+
+app.post("/counter/increment", function(req, res) {
+    counter++
+    res.status(201).json({ counter: counter });
+})
+
+app.post("/counter/decrement", function(req, res) {
+    counter--
+    res.status(201).json({ counter: counter });
+})
+
+app.post("/counter/double", function(req, res) {
+    counter*=2
+    res.status(201).json({ counter: counter });
+})
 //Export our app so other files can run it
 module.exports = app;
