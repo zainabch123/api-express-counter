2f722376e0d12aa9f28acb646fc59634191168d4
Zainab Choudhry <zainabchoudhry05@gmail.com>
2024-07-29 14:19:01 +0100

    Set up packages

---
+++ src/server.js
@@ -0,0 +1,19 @@
+//Include the express library
+const express = require("express");
+//Include the morgan middleware
+const morgan = require("morgan");
+//Include the cors middleware
+const cors = require("cors");
+
+//Create a new express application
+const app = express();
+
+//Tell express we want to use the morgan library
+app.use(morgan("dev"));
+//Tell express we want to use the cors library
+app.use(cors());
+//Tell express to parse JSON in the request body
+app.use(express.json());
+
+//Export our app so other files can run it
+module.exports = app;
